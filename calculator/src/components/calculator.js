932984aa44f001cf1ba362bc57b7304f182c9760
Keith March Mistler <kmistler@gmail.com>
2021-06-02 21:36:42 -0400

    added inputs and dropdown for calculator

--- calculator/src/components/calculator.js
+++ calculator/src/components/calculator.js
@@ -4,13 +4,36 @@ const Calculator = () => {
 
   const [number, setNumber] = useState('')
 
+  const updateNumber = (event) => {
+    setNumber(event.target.value)
+  }
+
   return(
     <div>
+
       <h1>Calculator</h1>
-            <div class="form-group">
-                <label class="num1">Number:</label>
-              <input type="text" name="search" onChange={(event) => setNumber(event.target.value)} value={number} /> 
-            </div>
+        <div class="form-group">
+          <input type="text" name="search" onChange={(event) => setNumber(event.target.value)} value={number} /> 
+        </div>
+<form>
+  <select name="calulate" id="calculate">
+    <option value="add">+</option>
+    <option value="subtract">-</option>
+    <option value="multiply">*</option>
+    <option value="divide">÷</option>
+  </select>
+</form>
+
+        <div class="form-group">
+          <input type="text" name="search" onChange={(event) => setNumber(event.target.value)} value={number} /> 
+        </div>
+
+        <div class="form-group">
+          <input type="text" name="search" onChange={(event) => setNumber(event.target.value)} value={number} /> 
+        </div>
+
+        
+
     </div>
           
   )
